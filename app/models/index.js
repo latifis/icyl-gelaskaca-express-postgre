deeedf8f572ce2122a52dc020744a2c639352f80
Latief Irfansyah <latifirfansyah@gmail.com>
2022-10-17 15:05:47 +0700

    fix setup

--- app/models/index.js
+++ app/models/index.js
@@ -1,4 +1,4 @@
-const config = require("../app/config/dbConfig.js");
+const config = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   {
     host: config.HOST,
     dialect: config.dialect,
-    operatorsAliases: false,
+    operatorsAliases: 0,
 
     pool: {
       max: config.pool.max,
@@ -24,8 +24,8 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.user = require("../models/userModel.js")(sequelize, Sequelize);
-db.role = require("../models/roleModel.js")(sequelize, Sequelize);
+db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
   through: "user_roles",
